3178a42173d198cfe7b08d211d823a79d110f6b8
Pokorski Sébastien <spokorski@gmail.com>
2025-02-08 20:29:57 +0100

    vite

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BobineLoader from './BobineLoader';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BobineLoader />
+  </React.StrictMode>
+);
